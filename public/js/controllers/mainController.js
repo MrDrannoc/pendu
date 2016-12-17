74fefe917a7b2d2479de09f480e521e68deb9ae7
MrDrannoc <poyer.k@gmail.com>
2016-12-17 22:37:01 +0100

    algo du pendu

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,14 +1,31 @@
 function mainController() {
 
     this.words = "flash";
+    this.proposition = "";
     this.arrayWords = this.words.split("");
+    this.correct = [];
+    this.count = 0;
 
-    this.compare = (proposition) => {
+    this.compare = () => {
+        this.correctA = this.correct.length;
+
+        this.proposition = this.proposition.toLowerCase();
+
         for (let letter of this.arrayWords) {
-            if (letter === proposition) {
-                this.response = proposition;
-                console.log(this.response);
+            if (letter == this.proposition) {
+                this.correct.push(this.proposition);
             }
+            this.correctB = this.correct.length;
         }
+
+        if (this.correctA == this.correctB) {
+            this.count++;
+        }
+
+
+        this.proposition = "";
+
+        console.log(this.correct);
+        console.log(this.count);
     };
 }
